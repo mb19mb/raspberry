4f66fb77ee8c3e64a4a85a687236756d150cf602
madmaddi <mopmop19@freenet.de>
2016-06-18 23:14:25 +0200

    initial

--- 017-tilt-angle-sensor/tilt-angle.py
+++ 017-tilt-angle-sensor/tilt-angle.py
@@ -4,23 +4,25 @@
 import RPi.GPIO as GPIO
 import time
 
-class Button(object):
+class TiltAngle(object):
     status = "off"
     vibPin = 14
     ledPin = 15
 
     def printOutput(self,null):
         print "Signal registered"
-        if self.status == "off":
-            GPIO.output(self.ledPin, GPIO.HIGH)
-            self.status = "on"
-        else:
-            GPIO.output(self.ledPin, GPIO.LOW)
-            self.status = "off"
-        
+        #if self.status == "off":
+        GPIO.output(self.ledPin, GPIO.HIGH)
+        time.sleep(0.1)
+        #    self.status = "on"
+        #else:
+        GPIO.output(self.ledPin, GPIO.LOW)
+
+        #    self.status = "off"
+
     def run(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.vibPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.vibPin, GPIO.IN)
         GPIO.setup(self.ledPin, GPIO.OUT)
         GPIO.output(self.ledPin, GPIO.LOW)
         GPIO.add_event_detect(self.vibPin, GPIO.FALLING, callback=self.printOutput, bouncetime=100)
@@ -34,6 +36,6 @@ class Button(object):
 
 
 if __name__ == "__main__":
-    b = Button()
-    b.run()
+    ta = TiltAngle()
+    ta.run()
 
