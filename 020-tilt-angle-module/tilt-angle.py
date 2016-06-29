902f0062c74f1df0428c86ca0fc95a76c5b99497
madmaddi <mopmop19@freenet.de>
2016-06-29 18:27:49 +0200

    up

--- 020-tilt-angle-module/tilt-angle.py
+++ 020-tilt-angle-module/tilt-angle.py
@@ -1,27 +1,23 @@
-# Benoetigte Module werden importiert und eingerichtet
+#/usr/bin/python
+
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
 
-# Hier wird der Eingangs-Pin deklariert, an dem der Sensor angeschlossen ist. Zusaetzlich wird auch der PullUP Widerstand am Eingang aktiviert
-GPIO_PIN = 14
-GPIO.setup(GPIO_PIN, GPIO.IN)
+pin = 14
+GPIO.setup(pin, GPIO.IN)
 
-print "Sensor-Test [druecken Sie STRG+C, um den Test zu beenden]"
+print "STRG+C to stop"
 
-# Diese AusgabeFunktion wird bei Signaldetektion ausgefuehrt
 def ausgabeFunktion(null):
     print("Signal erkannt")
 
-# Beim Detektieren eines Signals (fallende Signalflanke) wird die Ausgabefunktion ausgeloest
-GPIO.add_event_detect(GPIO_PIN, GPIO.FALLING, callback=ausgabeFunktion, bouncetime=100)
+GPIO.add_event_detect(pin, GPIO.FALLING, callback=ausgabeFunktion, bouncetime=100)
 
-# Hauptprogrammschleife
 try:
     while True:
         time.sleep(.2)
 
-# Aufraeumarbeiten nachdem das Programm beendet wurde
 except KeyboardInterrupt:
     GPIO.cleanup()
