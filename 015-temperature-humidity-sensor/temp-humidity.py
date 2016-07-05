bf42f0ad160c7f8170881bece4091aa9d0a7c389
madmaddi <mopmop19@freenet.de>
2016-07-05 18:55:13 +0200

    DHT22

--- 015-temperature-humidity-sensor/temp-humidity.py
+++ 015-temperature-humidity-sensor/temp-humidity.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python
 # coding=utf-8
 
-# Benoetigte Module werden importiert und eingerichtet
 import RPi.GPIO as GPIO
 import Adafruit_DHT
 import time
 
 sleeptime = 2
 
-# Sensor should be set to Adafruit_DHT.DHT11, Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
-DHTSensor = Adafruit_DHT.DHT11
+#DHTSensor = Adafruit_DHT.DHT11
 #DHTSensor = Adafruit_DHT.AM2302
+DHTSensor = Adafruit_DHT.DHT22
 
-GPIO_Pin = 15
+pin = 14
 
 print('STRG +C to stop')
 
 try:
     while(1):
+        humidity, temp = Adafruit_DHT.read_retry(DHTSensor, pin)
 
-        Luftfeuchte, Temperatur = Adafruit_DHT.read_retry(DHTSensor, GPIO_Pin)
-
-        print("-----------------------------------------------------------------")
-        if Luftfeuchte is not None and Temperatur is not None:
-            print('Temperatur = {0:0.1f}°C  | rel. Luftfeuchtigkeit = {1:0.1f}%'.format(Temperatur, Luftfeuchte))
+        if humidity is not None and temp is not None:
+            print('Temperatur = {0:0.1f}°C  | rel. Luftfeuchtigkeit = {1:0.1f}%'.format(temp, humidity))
         else:
-            print('Fehler beim Auslesen - Bitte warten auf nächsten Versuch!')
+            print('Fehler beim Auslesen ')
         print("-----------------------------------------------------------------")
         time.sleep(sleeptime)
 except KeyboardInterrupt:
