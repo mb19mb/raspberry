ca41aa950663f2193cde565173b42ec0854b9b29
mad maddi <mopmop19@freenet.de>
2016-10-08 08:21:14 +0000

    orig

--- permanent-humidity-measure/main.py
+++ permanent-humidity-measure/main.py
@@ -3,7 +3,7 @@ from tm1637 import *
 import time
 
 if __name__ == "__main__":
-    delay   = 5
+    delay   = 300 # 5min
     temp    = 0.0
     hum     = 0.0
 
@@ -18,16 +18,18 @@ if __name__ == "__main__":
         while(1):
 
             temp, hum = hm.readData()
-            print temp, hum
+            #print temp, hum
 
             temp = "{:.0f}".format(temp).split(".")[0][:2]  # get first two characters
             hum = "{:.0f}".format(hum).split(".")[0][:2]  # get first two characters
-            dataFile = open("./data", "a+")
-            dataFile.write(str(int(time.time())) + "#" + temp + "#" + hum + "\n")
+            dataFile = open("/home/pi/workspace/raspi-sensorkit/permanent-humidity-measure/data", "a+")
+            dataFile.write(str( int(time.time())) + "#" + temp + "#" + hum + "\n")
             dataFile.close()
             if len(temp) == 1: temp = "0" + temp
             if len(hum) == 1: hum = "0" + hum
 
+	    Display.Clear()
+	    Display.ShowDoublepoint(True)
             Display.Show1(0, int(temp[0]))
             Display.Show1(1, int(temp[1]))
             Display.Show1(2, int(hum[0]))
